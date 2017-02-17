efd040abf4157d63aa57296b4dc313e158afe232
elect <e.tmailbank@gmail.com>
2017-02-17 22:50:20 +0900

    2/17

--- chapter2.lisp
+++ chapter2.lisp
@@ -27,3 +27,75 @@
   (guess-my-number))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; local value and function ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(let ((a 5)
+      (b 6))
+  (+ a b))
+
+(flet ((f (n)
+	  (+ n 10)))
+   (f 5))
+
+(flet ((f (n)
+	 (+ n 10))
+       (g (n)
+	 (- n 3)))
+  (g (f 5))) 
+
+(labels ((a (n)
+	   (+ n 5))
+	 (b (n)
+	   (+ (a n) 6)))
+  (b 10))  ;;  this is for using internal local function 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; syntax ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(eq 'fooo 'fOoo) ;; -> T
+
+(+ 1 1.0)        ;; -> 2.0
+
+(expt 53 53)     ;; -> 2435......
+
+(/ 4 6)          ;; -> 2/3
+
+(/ 4.0 6)        ;; -> 0.66667
+
+(princ "Tutti Frutti")
+
+(princ "He yeiled \"stop tjat the\" from the busy street.")
+
+(expt 2 3)  ;; -> code mode
+
+'(expt 2 3) ;; -> data mode
+
+(cons 'children 'cat)
+
+(cons 'children 'nil)
+
+(cons 'children ())
+
+(cons 'pork '(beef children))
+
+(cons 'beef '(cons 'chicken ()))
+
+(cons 'pork (cons 'beef (cons 'chicken ())))
+
+(car '(pork beef chicken))
+
+(cdr '(pork beef chicken))
+
+(car (cdr '(pork beef chicken)))
+
+(cadr '(pork beef chicken))
+
+(list 'pork 'beef 'chicken)
+
+(car '((peas carrots tomatoes) (pork beef chicken)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
